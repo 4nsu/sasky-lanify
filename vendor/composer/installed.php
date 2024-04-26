044c1a8eae9d6f211b2896a2a755ebb4f1fb9575
ansu <annastiina.koivu@gmail.com>
2024-04-26 10:58:30 +0300

    luo sivupohjat

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '3f086a60950dbd2ad5d5285c4e5f811fa871f11d',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '3f086a60950dbd2ad5d5285c4e5f811fa871f11d',
+            'dev_requirement' => false,
+        ),
+        'league/plates' => array(
+            'pretty_version' => 'v3.5.0',
+            'version' => '3.5.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../league/plates',
+            'aliases' => array(),
+            'reference' => 'a6a3238e46c6e19af7318fdc36bfbe49b0620231',
+            'dev_requirement' => false,
+        ),
+    ),
+);
