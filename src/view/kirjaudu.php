98bcc7c0455847cbe95c996d749b63f02811726e
ansu <annastiina.koivu@gmail.com>
2024-05-08 10:51:13 +0300

    lisää kirjautumissivun

---
+++ src/view/kirjaudu.php
@@ -0,0 +1,23 @@
+<?php $this->layout('template', ['title' => 'Kirjautuminen']) ?>
+
+<h1>Kirjautuminen</h1>
+
+<!-- Varmistetaan että kirjautumistiedot lähetetää aina HTTPS-protokollan kautta,
+eli form action arvona kirjautumissivun tarkka osoite.
+Ei välttämätöntä neutronilla, joka ohjaa pyynnöt aina HTTPS kautta. -->
+<form action="https://<?= $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] ?>" method="POST">
+    <div>
+        <label>Sähköposti:</label>
+        <input type="text" name="email">
+    </div>
+    <div>
+        <label>Salasana:</label>
+        <input type="password" name="salasana">
+    </div>
+    <div class="error"><?= getValue($error,'virhe'); ?></div>
+    <div>
+        <input type="submit" name="laheta" value="Kirjaudu">
+    </div>
+</form>
+
+<div class="info">Jos sinulla ei ole vielä tunnuksia, niin voit luoda ne <a href="lisaa_tili">täällä</a>.</div>
