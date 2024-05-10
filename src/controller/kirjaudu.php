92c6276e43d0c4c1baf8cab65ddccea58b012fbb
ansu <annastiina.koivu@gmail.com>
2024-05-10 09:38:42 +0300

    korjaa kirjautumisen tarkistuksesta virheellisen rivin

--- src/controller/kirjaudu.php
+++ src/controller/kirjaudu.php
@@ -5,7 +5,6 @@ function tarkistaKirjautuminen($email="", $salasana="") {
     // Haetaan käyttäjän tiedot sen sähköpostiosoitteella. 
     require_once(MODEL_DIR . 'henkilo.php');
     $tiedot = haeHenkilo($email);
-    $tiedot = array_shift($tiedot);
 
     // Tarkistetaan ensin löytyikö käyttäjä. Jos löytyi, niin
     // tarkistetaan täsmäävätkö salasanat.
